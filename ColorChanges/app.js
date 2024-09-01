07c308127c54a21e72b0941fcb763689b97deed8
Puja Sah <psah10220528@gmail.com>
2024-09-01 09:18:22 +0530

    Add files via upload

---
+++ ColorChanges/app.js
@@ -0,0 +1,38 @@
+const buttons = document.querySelectorAll('.button');
+const body = document.querySelector("body");
+
+buttons.forEach(function (button){
+
+button.addEventListener('click', function(e){
+
+ if(e.target.id === 'grey'){
+  body.style.backgroundColor = e.target.id;
+ }
+ if(e.target.id === 'red'){
+  body.style.backgroundColor = e.target.id;
+ }
+ if(e.target.id === 'yellow'){
+  body.style.backgroundColor = e.target.id;
+ }
+ if(e.target.id === 'blue'){
+  body.style.backgroundColor = e.target.id;
+ }
+
+ if(e.target.id === 'purple'){
+  body.style.backgroundColor = e.target.id;
+ }
+})
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
