635ba942cdaf2c05a8796bafea5dabf020684bd8
Puja Sah <psah10220528@gmail.com>
2024-09-30 09:18:50 +0530

    Add files via upload

---
+++ module/library.js
@@ -0,0 +1,12 @@
+export { messege , user , test};
+
+let messege = "ES6 Modules";
+ function user(){
+    console.log("hello world");
+}
+
+ class test{
+    constructor(){
+        console.log("this is contructor method ");
+    }
+}
---
+++ module/main.js
@@ -0,0 +1,15 @@
+// import { messege , user , test}from "./library.js";
+import * as abc from "./library.js";
+
+ console.log(abc.messege);
+ // console.log(messege);
+
+
+//document.body.innerHTML = messege;
+
+
+abc.user();
+//.user();
+
+let a = new abc.test();
+//let a = new test();
