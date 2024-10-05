c2596ca78ae4ed684cf7edda9dd46a8ad3f451cd
Puja Sah <psah10220528@gmail.com>
2024-10-05 09:11:38 +0530

    Add files via upload

---
+++ Object js/app.js
@@ -0,0 +1,33 @@
+// Add Update values b
+const student = {
+ name: "puja",
+ city: "delhi",
+ gender: "male"
+
+}
+
+// student.city = "mumbai"      // updates 
+// student.gender = "female"  //  Add
+// student.mark  = [99, 38, 20];
+
+// delete student.mark      // delete 
+
+// nested object 
+const classinfo = {
+ sonu  : {
+  grade: "A",
+  city: "delhi"
+ },
+ puja :{
+  grade : "B",
+  city: "pune"
+ },
+ karan :{
+  grade : "C",
+  city: "dehradun"
+ },
+
+}
+// classinfo.sonu
+// classinfo.puja.city = "gurgawn"
+let random = Math.floor(Math.random() * 10 ) + 1;
---
+++ Object js/mathmethod.js
@@ -0,0 +1,10 @@
+// create random number between 1 and 100
+Math.floor(Math.random() * 10 ) + 1;
+Math.floor(Math.random() * 100 ) + 1;
+
+// create a random number between 1 to 5
+
+Math.floor(Math.random() * 5 ) + 1;
+
+// create num 20 to 25 
+Math.floor(Math.random() * 5 ) + 20;
