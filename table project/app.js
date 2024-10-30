cbf86958c6ae21a639b1c3ff3b9c71e1b57637ad
Puja Sah <psah10220528@gmail.com>
2024-10-30 16:49:07 +0530

    Add files via upload

---
+++ table project/app.js
@@ -0,0 +1,6 @@
+// print all tables of no
+let n = prompt("emter the number");
+n= parseInt(n);  // for convert n into integer values bcoz n is string 
+for(let i = n; i<=n*10; i=i+n){
+ document.write(i);
+}
